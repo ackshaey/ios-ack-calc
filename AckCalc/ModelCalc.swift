383f10f57053590d93015afa96c104e0828a74df
Ackshaey Singh <ackshaey@gmail.com>
2015-03-02 22:37:17 -0800

    Modularized functionality into model and added controller state dynamic properties

--- AckCalc/ModelCalc.swift
+++ AckCalc/ModelCalc.swift
@@ -5,43 +5,56 @@
 //  Created by Ackshaey Singh on 3/2/15.
 //  Copyright (c) 2015 Ackshaey Singh. All rights reserved.
 //
-
+//  +−×÷
 import Foundation
 
 class ModelCalc {
-    var numberStack : [Double] = []
+    var numberStack : [Double] = [0.0]
     var opStack : [String] = []
     
     func appendNumber(number : Double){
-        numberStack.append(number)
+        if(numberStack.count <= 2){
+            numberStack.append(number)
+        }
+        println("number stack is : \(numberStack)")
     }
     
     func appendOp(op : String){
         opStack.append(op)
+        println("op stack is : \(opStack)")
+    }
+    
+    func clear(){
+        opStack = []
+        numberStack = [0.0]
     }
     
     func evaluate() -> Double {
+        println("Evaluate : Number stack is \(numberStack) and count is \(numberStack.count)")
         var out = 0.0
-        let op = opStack.removeLast()
-        switch op{
-        case "+":
-            out = add()
-        case "*":
-            out = multiply()
-        case "/":
-            out = divide()
-        case "-":
-            out = subtract()
-        default:
-            out = 0.0
+        if (numberStack.count >= 2 && opStack.count >= 1){
+            let op = opStack.removeLast()
+            switch op{
+            case "+":
+                out = add()
+            case "×":
+                out = multiply()
+            case "÷":
+                out = divide()
+            case "−":
+                out = subtract()
+            default:
+                out = 0.0
+            }
         }
         return out
     }
-    //        +−×÷
+    
     
     private func add() -> Double{
         let a = numberStack.removeLast()
         let b = numberStack.removeLast()
+        println("a = \(a), b = \(b), out = \(a+b)")
         return a + b
     }
     
--- AckCalc/ViewController.swift
+++ AckCalc/ViewController.swift
@@ -5,21 +5,86 @@
 //  Created by Ackshaey Singh on 3/2/15.
 //  Copyright (c) 2015 Ackshaey Singh. All rights reserved.
 //
+//  +−×÷
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    var isTypingDigit = false // State of digit press
+    var isTypingOp = false // State of op press
+    var typingDigit : Bool{ // Call when digit pressed
+        get{
+            return isTypingDigit
+        }
+        set{
+            isTypingDigit = newValue
+            isTypingOp = !newValue
+        }
+    }
+    var typingOp : Bool{ // Call when op pressed
+        get{
+            return isTypingOp
+        }
+        set{
+            isTypingDigit = !newValue
+            isTypingOp = newValue
+        }
+    }
     
+    let modelCalc = ModelCalc()
     
+    var displayString : String {
+        get{
+            return displayLabel.text!
+        }
+        set{
+            displayLabel.text = newValue
+        }
+    }
     
+    var displayDouble : Double {
+        get {
+            return NSString(string: displayLabel.text!).doubleValue
+        }
+        set {
+            let str = NSString(format: "%.2f", newValue)
+            displayString = str
+        }
+    }
     
+    @IBOutlet weak var displayLabel: UILabel!
     
+    @IBAction func keyOperation(sender: UIButton) {
+        if(!typingOp){
+            typingOp = true // sets typing number to false
+            modelCalc.appendNumber(displayDouble)
+            modelCalc.appendOp(sender.currentTitle!)
+        }
+    }
     
+    @IBAction func keyDigit(sender: UIButton) {
+        let numberStr = sender.currentTitle!
+        if(typingDigit){
+            displayString = displayString + numberStr
+        } else{
+            displayString = numberStr
+        }
+        typingDigit = true // sets typing op to false
+    }
     
+    @IBAction func keyEnter() {
+        modelCalc.appendNumber(displayDouble)
+        let out = modelCalc.evaluate()
+        displayDouble = out
+    }
     
-    
-    
+    @IBAction func keyClear() {
+        isTypingDigit = false
+        isTypingOp = false
+        displayString = "0.00"
+        modelCalc.clear()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
